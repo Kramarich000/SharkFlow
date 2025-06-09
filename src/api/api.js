3236eb2499a3ae825de68c86793b7efeca398835
Karen <karen.avakov2@gmail.com>
2025-06-09 22:40:55 +0300

    поправил адрес

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://taskflow-api-gavu.onrender.com'
+    : 'http://localhost:8080';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL,
 });
 
 export default api;
